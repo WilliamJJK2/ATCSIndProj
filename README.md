08216e22fcfc0b4745d73664de290bafd3ce2fa7
William Kim <wkim25@students.polytechnic.org>
2024-05-23 11:52:26 -0700

    Created the deck class and added to the README file

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 This is my final project for the 2024 Advanced Topics in CS course at Polytechnic School
 
 Day 2: I am deciding between a couple of ideas that I want to do. I wanted to try to figure out how to create a card game such as blackjack or poker, or I could create a system that stores and measures data depending on certain circumstances. After deliberating between these two choices, I believe that the second idea was too basic in the idea, so I am deciding to create a blackjack game.
+
+Day 3: Today, I focused on developing a card and deck class for my independent project. I started by creating the Card class, which includes attributes for the card's rank and suit, along with methods to retrieve those values. Afterwards, I designed the Deck class, which creates a standard deck of 52 cards and puts the correct value on each card such as the named cards so that they conform with the rules of Blackjack. I also added a shuffling version in order to not have the cards in a chronological order. I have successfully implemented and tested these classes during class.
+
+Day 4:
--- project.py
+++ project.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
 """
-deck.py
-The deck for my BlackJack Game made for my Independent Project
+project.py
+The BlackJack Game made for my Independent Project
 """
 
 __author__ = "William Kim"
 __version__ = "2024-05-17"
+
+import random
 
 class Card():
 
@@ -19,19 +21,44 @@ class Card():
     
     def get_rank_value(self):
         ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
-        return ranks.index(self.rank) + 1
+        if ranks[ranks.index(self.rank)] == "Jack" or "Queen" or "King":
+            return 10
+        elif ranks[self.rank] == "Ace":
+            return 11
+        else:
+            return ranks.index(self.rank) + 1
     
     def get_suit(self):
         return self.suit
     
-    def __str__(self):
-        return card.get_rank() + " of " + card.get_suit()
-    
-card = Card("Ace", "Spades")
-print(card.get_rank())
-print(card.get_rank_value())
-print(card.get_suit())
-print(card)
 
+class Deck():
 
-        
+    def __init__(self):
+        self.cards = []
+        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append(Card(rank, suit))
+        self.shuffle()
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def draw_card(self):
+        if not self.cards:
+            print("The deck is empty.")
+            return None
+        return self.cards.pop(0)
+
+def main():
+    card = Card("Ace", "Spades")
+    print(card.get_rank())
+    print(card.get_rank_value())
+    print(card.get_suit())
+    print(card.get_rank() + " of " + card.get_suit())
+
+if __name__ == "__main__":
+    main()
